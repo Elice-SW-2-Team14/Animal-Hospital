f239a5ec7e5c8abf16eeae8c9bd6eb4c5bd3715e
hovelopin <kimhojin3714@naver.com>
2022-07-15 13:21:47 +0900

    [#147]fix: 머지 충돌 해결

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Routes, BrowserRouter, Route} from 'react-router-dom';
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
 
 import Login from './pages/login/LoginLayout';
 import Register from './pages/register/Register';
@@ -10,21 +10,22 @@ import HospitalMypage from './pages/hospital-mypage/HospitalMypage';
 import HospitalInfo from './pages/hospital-info/HospitalInfo';
 import AdminMypage from './pages/admin-mypage/AdminMypage';
 
-
 function App() {
-  return <BrowserRouter>
-    <Routes>
-      {/* <Route path='/' element={< />} />  */}
-      <Route path='/login' element={<Login/>} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/user-mypage' element={<UserMypage />} />
-      <Route path='/user-info' element={<UserInfo />} />
-      <Route path='/pet-info' element={<PetInfo />} />
-      <Route path='/hospital-mypage' element={<HospitalMypage />} />
-      <Route path='/hospital-info' element={<HospitalInfo />} />
-      <Route path='/admin-mypage' element={<AdminMypage />} />
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path='/' element={< />} />  */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/user-mypage" element={<UserMypage />} />
+        <Route path="/user-info" element={<UserInfo />} />
+        <Route path="/pet-info" element={<PetInfo />} />
+        <Route path="/hospital-mypage" element={<HospitalMypage />} />
+        <Route path="/hospital-info" element={<HospitalInfo />} />
+        <Route path="/admin-mypage" element={<AdminMypage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
